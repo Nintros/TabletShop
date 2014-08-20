03dcd4790e318423a7f6018a87670eac09ef61d8
Maksym Mokrousov <mmokrous@softserveinc.com>
2014-08-20 23:03:01 +0300

    Added  forgotten constants file

---
+++ TabletShop/TabletShop/Helpers/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TabletShop.Helpers
+{
+	public static class Constants
+	{
+		public const string WCF_SERVICE_URL = @"http://localhost:8733/rest.svc/";
+	}
+}
